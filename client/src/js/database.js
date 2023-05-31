f81227ea95164a40d2a4150cd6f111f50c2226a6
Kaiden Parcher <ktparcher@.com>
2023-05-30 19:01:47 -0700

    debug

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,39 +1,35 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => { 
-  console.error('putDb not implemented');
-  console.log('PUT request to the database received');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  const request = store.put({id: 1, value: content });
+export const putDb = async (content) => {
+  console.log("PUT to the database");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ value: content });
   const result = await request;
-  console.log('Data has been saved', result);
-  };
+  console.log("data saved to the database", result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('User need to utilize getDb');
-  console.log('GET request to the database received');
-  const jateDb = await openDB('jate', 1);
-  const text = jateDb.transaction('jate', 'readonly');
-  const store = text.objectStore('jate');
-  const request = store.get(1);
+  console.log("GET from the database");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
   const result = await request;
-  console.log('result.value', result);
+  console.log("result.value", result);
   return result?.value;
 };
 
